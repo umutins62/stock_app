aae2b5bb2a0a771ccf8556848282b1eba8df1e93
umutins62 <umutins62@hotmail.com>
2023-09-28 15:42:41 +0300

    feat:tables price and amount calculate total value

---
+++ stock/filters.py
@@ -0,0 +1,9 @@
+# filters.py
+
+from django import template
+
+register = template.Library()
+
+@register.filter(name='multiply')
+def multiply(value, arg):
+  return value * arg
--- stock/views.py
+++ stock/views.py
@@ -1,5 +1,9 @@
 from django.shortcuts import render
 from stock.models import Stock, Transaction, MoneyTransaction, GeneralSettings
+from openpyxl import load_workbook
+import pandas as pd
+
+
 
 # Create your views here.
 def index(request):
@@ -8,11 +12,18 @@ def index(request):
     money_transactions = MoneyTransaction.objects.all()
     general_settings = GeneralSettings.objects.all()
 
+    excel_file = 'stock.xlsx'
+    excel_data = pd.read_excel(excel_file)
+    print(excel_data)
+    deger=1.00
+
+
     context = {
         'stocks': stocks,
         'transactions': transactions,
         'money_transactions': money_transactions,
         'general_settings': general_settings,
+        'deger':deger,
     }
 
     return render(request, "index.html", context=context)
